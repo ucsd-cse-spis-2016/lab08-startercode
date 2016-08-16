4a9babfe7eb406a77a2af48136440718a558c7ca
Hoang Nguyen <pi@spispi-06.(none)>
2016-08-16 15:23:39 -0700

    correct version

--- 01_blinking_led.py
+++ 01_blinking_led.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 # Import the relevant libraries
 import RPi.GPIO as GPIO
 import time
@@ -7,28 +7,28 @@ LedPin = 11    # pin11
 
 def setup():
         ''' One time set up configurations'''
-	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
-	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+        GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+        GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
+        GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
 
 def loop():
-	while True:
+        while True:
                 # This code repeats forever
-		print '...led on'
-		GPIO.output(LedPin, GPIO.LOW)  # led on
-		time.sleep(0.5)
-		print 'led off...'
-		GPIO.output(LedPin, GPIO.HIGH) # led off
-		time.sleep(0.5)
+                print '...led on'
+                GPIO.output(LedPin, GPIO.LOW)  # led on
+                time.sleep(0.5)
+                print 'led off...'
+                GPIO.output(LedPin, GPIO.HIGH) # led off
+                time.sleep(0.5)
 
 def destroy():
-	GPIO.output(LedPin, GPIO.HIGH)     # led off
-	GPIO.cleanup()                     # Release resource
+        GPIO.output(LedPin, GPIO.HIGH)     # led off
+        GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
+        setup()
+        try:
+                loop()
+        except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+                destroy()
 
