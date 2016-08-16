0f02b15b659fa59a3d02365cbe727b158c3cb508
Diba Mirza <dimirza@eng.ucsd.edu>
2016-08-15 23:02:34 -0700

    Blinking led

--- 01_blinking_led.py
+++ 01_blinking_led.py
@@ -6,11 +6,13 @@ import time
 LedPin = 11    # pin11
 
 def setup():
+        ''' One time set up configurations'''
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
 	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
 
 def loop():
+        ''' This code repeats forever'''
 	while True:
 		print '...led on'
 		GPIO.output(LedPin, GPIO.LOW)  # led on
